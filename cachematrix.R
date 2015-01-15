55eea97a192fb6d777034d4a81e119a46b1b1b0f
Matthieu Masquelet (MM) <matthieu.masquelet@gmail.com>
2015-01-15 03:19:55 -0500

    Completed assingment 2
    Wrote functions and comments. Tested it successfully on a 3x3 matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to extend the regular matrix class by allowing the matrix inverse
+## to be cached for easy retrieval later on
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a regular matrix and construct a new object that stores
+## its inverse alongside. Methods are also provided to set/get values for the 
+## matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xinv <- NULL
+    set <- function(y) {
+        x <<- y
+        xinv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) xinv <<- inv
+    getinv <- function() xinv
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve efficiently returns the inverse of the extended matrix x. If the
+## inverse of x is already known it returns it directly. If not it computes it
+## and stores it in x for easy retrieval later on. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    xinv <- x$getinv()
+    if (!is.null(xinv)) {
+        message("getting cached data")
+        return(xinv)
+    }
+    mat <- x$get()
+    xinv <- solve(mat, ...)
+    x$setinv(xinv)
+    xinv
 }
